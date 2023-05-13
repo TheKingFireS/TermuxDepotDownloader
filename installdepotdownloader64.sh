a55a8d567608f7d3ff97ec76e3941d2697c9cf9b
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-05-13 18:04:56 +0200

    Update and rename installdepotdownloader.sh to installdepotdownloader64.sh

--- installdepotdownloader64.sh
+++ installdepotdownloader64.sh
@@ -1,3 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/sh
+# Only use this if you're in PRoot.
 apt update && apt upgrade -y
 apt install wget libicu67 -y
 wget "https://download.visualstudio.microsoft.com/download/pr/e7866e12-a380-4994-9c56-1bd3a1e0a546/22a5e54cb4e637c5aac7ec6dcab0d739/dotnet-runtime-6.0.16-linux-arm64.tar.gz"
