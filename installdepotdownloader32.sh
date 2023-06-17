bc2c77292ecc9a68dbe65d2c4a6e092688096d77
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-06-17 19:36:45 +0200

    Update and fix mistake in installdepotdownloader32.sh

--- installdepotdownloader32.sh
+++ installdepotdownloader32.sh
@@ -1,6 +1,6 @@
-#!/data/data/com.termux/files/usr/bin/sh
+#!/bin/sh
 # Only use this if you're in PRoot.
-apt update && apt upgrade -y
+apt update && apt full-upgrade -y
 apt install wget libicu67 -y
 wget "https://download.visualstudio.microsoft.com/download/pr/7039aeff-2d14-46b3-a560-e5af5591d6c6/3cb3a4aae10e161413fda1100007551a/dotnet-runtime-6.0.16-linux-arm.tar.gz"
 mkdir dotnet && cd dotnet
