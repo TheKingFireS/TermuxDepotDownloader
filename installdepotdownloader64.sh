3a07d0605ef1536ab52452563aadfcc6dac81b3e
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-06-17 19:41:15 +0200

    Update libicu for new Debian version in installdepotdownloader64.sh

--- installdepotdownloader64.sh
+++ installdepotdownloader64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Only use this if you're in PRoot.
 apt update && apt full-upgrade -y
-apt install wget libicu67 -y
+apt install wget libicu72 -y
 wget "https://download.visualstudio.microsoft.com/download/pr/e7866e12-a380-4994-9c56-1bd3a1e0a546/22a5e54cb4e637c5aac7ec6dcab0d739/dotnet-runtime-6.0.16-linux-arm64.tar.gz"
 mkdir dotnet && cd dotnet
 tar -xf ../dotnet-runtime-6.0.16-linux-arm64.tar.gz
