28a6a1abf985e02152b85dc261c6c1308a65ba35
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-06-21 21:07:26 +0200

    Update installproot.sh

--- installproot.sh
+++ installproot.sh
@@ -3,3 +3,4 @@
 pkg upgrade -y 
 pkg install proot-distro -y
 proot-distro install debian
+ln -s /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root/ /data/data/com.termux/files/home/droot
