4f618bf4ef91d6987a285e962ff32c5bc49e8c9c
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-06-21 18:16:35 +0200

    Delete installdepotdownloader32.sh

--- installdepotdownloader32.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Only use this if you're in PRoot.
-apt update && apt full-upgrade -y
-apt install wget libicu72 -y
-wget "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm.zip"
-mkdir depotdownloader && cd depotdownloader
-unzip ../DepotDownloader-linux-arm.zip
-chmod u+x DepotDownloader
