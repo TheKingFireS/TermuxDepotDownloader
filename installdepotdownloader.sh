002b622e026eb94cc0da52bdf491bb8c183f8d0e
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-07-06 04:27:12 +0200

    Update installdepotdownloader.sh

--- installdepotdownloader.sh
+++ installdepotdownloader.sh
@@ -9,10 +9,10 @@ if [[ "$arch" == x86_64* ]]; then
 elif [[ "$arch" == i*86 ]]; then
     echo "X86_32 Architecture, unsupported."
     exit
-elif  [[ "$arch" == armv7l ]]; then
+elif [[ "$arch" == armv7l ]]; then
     echo "ARM32 Architecture"
     wget -O "DepotDownloader-linux.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm.zip"
-elif  [[ "$arch" == aarch64 ]]; then
+elif [[ "$arch" == aarch64 ]]; then
     echo "ARM64 Architecture"
     wget -O "DepotDownloader-linux.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm64.zip"
 fi
