54120d76d1f9d9b7adcb750de133b58accedf17b
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-08-07 18:35:15 +0200

    Update installdepotdownloader.sh

--- installdepotdownloader.sh
+++ installdepotdownloader.sh
@@ -4,17 +4,17 @@ arch=$(arch)
 apt update && apt full-upgrade -y
 apt install wget libicu72 -y
 if [[ "$arch" == x86_64* ]]; then
-    echo "X86_64 Architecture"
-    wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-x64.zip"
-elif [[ "$arch" == i*86 ]]; then
-    echo "X86_32 Architecture, unsupported."
-    exit
+	echo "X86_64 Architecture"
+	wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-x64.zip"
 elif [ "$arch" == "armv7l" ]; then
-    echo "ARM32 Architecture"
-    wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm.zip"
+	echo "ARM32 Architecture"
+	wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm.zip"
 elif [ "$arch" == "aarch64" ]; then
-    echo "ARM64 Architecture"
-    wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm64.zip"
+	echo "ARM64 Architecture"
+	wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm64.zip"
+else
+	echo "No supported architectures detected, exiting..."
+	exit
 fi
 mkdir depotdownloader && cd depotdownloader || exit
 unzip ../DepotDownloader.zip
