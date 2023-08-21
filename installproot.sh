d26417d909e632b084e87585af08def34e0d17de
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-08-22 00:55:25 +0200

    Update installproot.sh

--- installproot.sh
+++ installproot.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/sh
-# Download and install proot debian
+# Download and install proot alpine
 pkg upgrade -y 
 pkg install proot-distro -y
 proot-distro install alpine
-ln -s /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root/ /data/data/com.termux/files/home/droot
+ln -s /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/alpine/root/ /data/data/com.termux/files/home/droot
