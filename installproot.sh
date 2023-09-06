11c19a2833a1e2e32f23761766869386a0d2eaf8
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-09-06 21:17:06 +0200

    Avoiding questions about replacing files.

--- installproot.sh
+++ installproot.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/sh
 # Download and install proot debian
-pkg upgrade -y 
+pkg upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install proot-distro -y
 proot-distro install debian
 ln -s /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root/ /data/data/com.termux/files/home/droot
