c0712d38314443bcd60cfed44628b1bf440b166c
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-09-19 16:00:57 +0200

    Added useless files to ``rm``

--- installdepotdownloader.sh
+++ installdepotdownloader.sh
@@ -9,4 +9,4 @@ apk upgrade && apk add dotnet6
 curl -LO "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/depotdownloader-2.5.0.zip"
 mkdir depotdownloader && cd depotdownloader || exit
 unzip ../depotdownloader-2.5.0.zip
-rm ../depotdownloader-2.5.0.zip
+rm ../depotdownloader-2.5.0.zip DepotDownloader.exe DepotDownloader.pdb
