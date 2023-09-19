19bc532687b02e6285828f5e5e2de6f3c686f649
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-09-19 13:24:47 +0200

    upstream

--- installproot.sh
+++ installproot.sh
@@ -1,6 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/sh
 # Download and install proot alpine
-pkg upgrade -y 
+pkg upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install proot-distro -y
 proot-distro install alpine
 ln -s /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/alpine/root/ /data/data/com.termux/files/home/droot
