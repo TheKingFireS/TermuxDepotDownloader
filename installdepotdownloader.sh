8c7210cf2583f41589c076187de6254f441ba568
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2023-09-19 13:24:07 +0200

    upstream

--- installdepotdownloader.sh
+++ installdepotdownloader.sh
@@ -3,9 +3,10 @@
 arch=$(arch)
 if [ "$arch" = "i686" ]; then
 	echo "X86_32 architecture detected, unsupported."
-	exit
+	exit 1
 fi
 apk upgrade && apk add dotnet6
 curl -LO "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/depotdownloader-2.5.0.zip"
 mkdir depotdownloader && cd depotdownloader || exit
 unzip ../depotdownloader-2.5.0.zip
+rm ../depotdownloader-2.5.0.zip
