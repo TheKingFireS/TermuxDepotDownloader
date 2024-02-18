6e3b560c4b753889b86760caecb868f300231553
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-02-18 17:50:14 +0100

    Update installdepotdownloader.sh

--- installdepotdownloader.sh
+++ installdepotdownloader.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env sh
 # Only use this if you're in PRoot.
 arch=$(arch)
 apt update && apt full-upgrade -y
 apt install wget libicu72 -y
-if [ "$arch" == "x86_64" ]; then
+if [ "$arch" = "x86_64" ]; then
 	echo "X86_64 Architecture"
 	wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-x64.zip"
-elif [[ "$arch" == armv*l ]]; then
+elif echo "$arch" | grep -q 'armv[7-9]l$'; then
 	echo "ARM32 Architecture"
 	wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm.zip"
-elif [ "$arch" == "aarch64" ]; then
+elif [ "$arch" = "aarch64" ]; then
 	echo "ARM64 Architecture"
 	wget -O "DepotDownloader.zip" "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/DepotDownloader-linux-arm64.zip"
 else
-	echo "No supported architectures detected, exiting..."
+	echo "Unsupported ""$arch"" architecture detected, exiting..."
 	exit 1
 fi
 mkdir depotdownloader && cd depotdownloader || exit
