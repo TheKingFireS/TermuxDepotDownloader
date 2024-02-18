c0eeae61dad8d8e4af0e80331f1230be013d08c6
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-02-18 14:56:17 +0100

    Fixing my mistake in installdepotdownloader.sh
    use wget instead of curl
    add ``apk update`` just incase
    change ``#/bin/ash`` to ``#!/usr/bin/env sh`` for POSIX compliant and portability

--- installdepotdownloader.sh
+++ installdepotdownloader.sh
@@ -1,12 +1,12 @@
-#!/bin/ash
+#!/usr/bin/env sh
 # Only use this if you're in PRoot.
 arch=$(arch)
 if [ "$arch" = "i686" ]; then
 	echo "X86_32 architecture detected, unsupported."
 	exit 1
 fi
-apk upgrade && apk add dotnet6
-curl -LO "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/depotdownloader-2.5.0.zip"
+apk update && apk upgrade && apk add dotnet6-runtime
+wget "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.5.0/depotdownloader-2.5.0.zip"
 mkdir depotdownloader && cd depotdownloader || exit
 unzip ../depotdownloader-2.5.0.zip
 rm ../depotdownloader-2.5.0.zip DepotDownloader.exe DepotDownloader.pdb
