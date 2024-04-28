160ccf3d45b9bbc988ec82c5b939be36f0d2e289
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-04-28 21:48:55 +0200

    Update installproot.sh

--- installproot.sh
+++ installproot.sh
@@ -1,5 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/env sh
 # Download and install proot alpine
+arch=$(uname -m)
+if echo "$arch" | grep -q 'i[3-6]86$'; then
+	echo "Unsupported X86 32bit architecture detected, exiting..."
+	exit 1
+fi
 pkg upgrade -y -o Dpkg::Options::="--force-confold"
 pkg install proot-distro -y
 proot-distro install alpine
