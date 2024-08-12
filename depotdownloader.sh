8da10a8475846dc589cd8422925f23eb639a6cd6
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-08-12 03:56:05 +0200

    depotdownloader.sh: New file

--- depotdownloader.sh
+++ depotdownloader.sh
@@ -1,2 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/env sh
 #WIP
+username="user"
+proot-distro login debian --user $username --bind $HOME/storage/downloads/depotdownloaded:/home/$username --shared-tmp -- DepotDownloader "$@" || exit
+rm -rf $HOME/storage/downloads/depotdownloaded/.local
