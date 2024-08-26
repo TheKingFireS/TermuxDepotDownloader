2a1d0e587427087321b486423b90bd5a71953e65
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-08-26 19:34:13 +0200

    depotdownloader.sh: added something

--- depotdownloader.sh
+++ depotdownloader.sh
@@ -6,7 +6,14 @@ print_green() {
 	#Bold bright green
 	printf "\033[1;92m$1\033[0m\n"
 }
+print_red() {
+	#Bold red
+	printf "\033[1;31m$1\033[0m\n"
+}
 #end of setting env
-proot-distro login debian --user $username --bind "$HOME"/storage/downloads/depotdownloaded:/home/$username --shared-tmp -- DepotDownloader "$@" || exit
-rm -rf "$HOME"/storage/downloads/depotdownloaded/.local
-print_green "Download finished! Check the 'Downloads/depotdownloaded' folder in your file manager app."
+proot-distro login debian --user $username --bind "$HOME"/storage/downloads/depotdownloaded:/home/$username --shared-tmp -- DepotDownloader "$@"
+if [ $? -eq 0 ]; then
+	print_green "Download finished! Check the 'Downloads/depotdownloaded' folder in your file manager app."
+else
+	print_red "Failed"
+fi
