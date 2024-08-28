ce095f88ff9134fc5796670309daaaac9949ed09
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-08-28 07:29:36 +0200

    installdepotdownloader.sh: remove duplicated

--- installdepotdownloader.sh
+++ installdepotdownloader.sh
@@ -10,16 +10,12 @@ dlfile() {
 	curl --retry 10 --retry-delay 2 --retry-all-errors -Lo "DepotDownloader.zip" "$url"
 }
 if [ "$arch" = "x86_64" ]; then
-	echo "X86_64 Architecture"
 	installersetup
 	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.6.0/DepotDownloader-linux-x64.zip"
 elif echo "$arch" | grep -q 'armv[7-9]l$'; then
-	echo "ARM32 Architecture"
 	installersetup
 	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.6.0/DepotDownloader-linux-arm.zip"
 elif [ "$arch" = "aarch64" ]; then
-	echo "Added 'export DOTNET_GCHeapHardLimit=1C0000000' workaround"
-	echo "ARM64 Architecture"
 	installersetup
 	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.6.0/DepotDownloader-linux-arm64.zip"
 else
