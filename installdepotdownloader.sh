c98c500600c99aecdb2f20db57ae9179677e676d
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-09-07 09:46:26 +0200

    Update installdepotdownloader.sh

--- installdepotdownloader.sh
+++ installdepotdownloader.sh
@@ -11,13 +11,13 @@ dlfile() {
 }
 if [ "$arch" = "x86_64" ]; then
 	installersetup
-	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.6.0/DepotDownloader-linux-x64.zip"
+	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.1/DepotDownloader-linux-x64.zip"
 elif echo "$arch" | grep -q 'armv[7-9]l$'; then
 	installersetup
-	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.6.0/DepotDownloader-linux-arm.zip"
+	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.1/DepotDownloader-linux-arm.zip"
 elif [ "$arch" = "aarch64" ]; then
 	installersetup
-	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.6.0/DepotDownloader-linux-arm64.zip"
+	dlfile "https://github.com/SteamRE/DepotDownloader/releases/download/DepotDownloader_2.7.1/DepotDownloader-linux-arm64.zip"
 else
 	echo "Unsupported ""$arch"" architecture detected, exiting..."
 	exit 1
