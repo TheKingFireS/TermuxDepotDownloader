3ba1933937904df28e288d40542c86dc12e9cd3a
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-10-25 17:47:14 +0200

    installproot.sh: improve codestyle

--- installproot.sh
+++ installproot.sh
@@ -1,9 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/env sh
 # Download and install proot alpine (WIP)
-# setting env
+
+# setting env var
 arch=$(dpkg --print-architecture)
 username="user"
+installed_rootfs="$PREFIX/var/lib/proot-distro/installed-rootfs"
+# end of setting env var
 
+# setting function
 installersetup() {
 	#update and installing required termux packages
 	pkg upgrade -y -o Dpkg::Options::="--force-confold"
@@ -14,13 +18,13 @@ installersetup() {
 	proot-distro login alpine --shared-tmp -- apk upgrade
 	proot-distro login alpine --shared-tmp -- apk add doas icu-libs
 	proot-distro login alpine --shared-tmp -- apk cache clean
-	rm $PREFIX/var/lib/proot-distro/installed-rootfs/alpine/var/cache/apk/*.apk
+	rm "$installed_rootfs"/alpine/var/cache/apk/*.apk
 	#add user
 	proot-distro login alpine --shared-tmp -- addgroup storage
 	proot-distro login alpine --shared-tmp -- adduser -G wheel -D $username
 	proot-distro login alpine --shared-tmp -- adduser $username storage
-	echo "permit nopass :wheel as root" > $PREFIX/var/lib/proot-distro/installed-rootfs/alpine/etc/doas.d/doas.conf
-	chmod u-w  $PREFIX/var/lib/proot-distro/installed-rootfs/alpine/etc/doas.d/doas.conf
+	echo "permit nopass :wheel as root" > "$installed_rootfs"/alpine/etc/doas.d/doas.conf
+	chmod u-w  "$installed_rootfs"/alpine/etc/doas.d/doas.conf
 	#setup storage and directory for depotdownloader
 	if [ ! -d "$HOME/storage" ]; then
 		termux-setup-storage
@@ -29,7 +33,7 @@ installersetup() {
 		done
 	fi
 	if [ ! -d "$HOME/storage/downloads/depotdownloaded" ]; then
-		mkdir $HOME/storage/downloads/depotdownloaded
+		mkdir "$HOME"/storage/downloads/depotdownloaded
 	fi
 }
 
@@ -37,14 +41,14 @@ dlfile() {
 	#download file and extract file to alpine's bin executable directory
 	url="$1"
 	curl --retry 10 --retry-delay 2 --retry-all-errors -Lo "DepotDownloader.zip" "$url"
-	unzip -j DepotDownloader.zip "DepotDownloader" -d $PREFIX/var/lib/proot-distro/installed-rootfs/alpine/usr/bin
+	unzip -j DepotDownloader.zip "DepotDownloader" -d "$installed_rootfs"/alpine/usr/bin
 	rm DepotDownloader.zip
-	chmod u+x $PREFIX/var/lib/proot-distro/installed-rootfs/alpine/usr/bin/DepotDownloader
+	chmod u+x "$installed_rootfs"/alpine/usr/bin/DepotDownloader
 	#download wrapper file for depotdownloader
-	curl --retry 10 --retry-delay 2 --retry-all-errors -Lo $PREFIX/bin/depotdownloader "https://raw.githubusercontent.com/TheKingFireS/TermuxDepotDownloader/alpine/depotdownloader.sh"
-	chmod +x $PREFIX/bin/depotdownloader
+	curl --retry 10 --retry-delay 2 --retry-all-errors -Lo "$PREFIX"/bin/depotdownloader "https://raw.githubusercontent.com/TheKingFireS/TermuxDepotDownloader/alpine/depotdownloader.sh"
+	chmod +x "$PREFIX"/bin/depotdownloader
 }
-# end of setting env
+# end of setting function
 
 if [ "$arch" = "x86_64" ]; then
 	echo "X86_64 Architecture"
@@ -58,7 +62,7 @@ elif [ "$arch" = "aarch64" ]; then
 	echo "ARM64 Architecture"
 	echo "Added 'GC heap initialization failed with error 0x8007000E' workaround"
 	installersetup
-	echo "export DOTNET_GCHeapHardLimit=1C0000000" > $PREFIX/var/lib/proot-distro/installed-rootfs/alpine/etc/profile.d/dotnet.sh
+	echo "export DOTNET_GCHeapHardLimit=1C0000000" > "$installed_rootfs"/alpine/etc/profile.d/dotnet.sh
 	dlfile "https://github.com/TheKingFireS/TermuxDepotDownloader/releases/download/selfcontained-alpine/DepotDownloader-linux-musl-arm64-unofficial.zip"
 #Added it, will uncomment when it get supported.
 #elif [ "$arch" = "riscv64" ]; then
