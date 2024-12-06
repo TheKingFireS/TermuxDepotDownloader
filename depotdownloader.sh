632a794d586ac2ce46fc24f72bbe311b2e9d1129
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2024-12-06 06:52:08 +0100

    depotdownloader.sh: don't print out success if no arguments were provided.

--- depotdownloader.sh
+++ depotdownloader.sh
@@ -4,16 +4,18 @@
 username="user"
 print_green() {
 	#Bold bright green
-	printf "\033[1;92m$1\033[0m\n"
+	printf "\033[1;92m%s\033[0m\n" "$1"
 }
 print_red() {
 	#Bold red
-	printf "\033[1;31m$1\033[0m\n"
+	printf "\033[1;31m%s\033[0m\n" "$1"
 }
 #end of setting env
 proot-distro login alpine --user $username --shared-tmp -- ash -c "cd /storage/emulated/0/Download/depotdownloaded && DepotDownloader \"\$@\"" -- "$@"
 if [ $? -eq 0 ]; then
-	print_green "Download finished! Check the 'Downloads/depotdownloaded' folder in your file manager app."
+	if [ "$#" -ne 0 ]; then
+		print_green "Download finished! Check the 'Downloads/depotdownloaded' folder in your file manager app."
+	fi
 else
 	print_red "Failed"
 fi
