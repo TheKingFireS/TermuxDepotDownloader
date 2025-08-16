dd11f947158803a6c0ba4f6f8e10980202339205
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2025-08-16 02:02:16 +0200

    depotdownloader.sh: Add new env var "SET_DIR" (i hope it won't break)

--- depotdownloader.sh
+++ depotdownloader.sh
@@ -2,6 +2,8 @@
 # WIP
 # setting env var
 username="user"
+# SET_DIR: will only work if you provided path from proot and not Termux. Yes, full path.
+SET_DIR="${SET_DIR:-/storage/emulated/0/Download/depotdownloaded}"
 # end of setting env var
 # setting function
 print_green() {
@@ -13,10 +15,10 @@ print_red() {
 	printf "\033[1;31m%s\033[0m\n" "$1"
 }
 # end of setting function
-proot-distro login alpine --user $username --no-arch-warning --shared-tmp -- ash -c "cd /storage/emulated/0/Download/depotdownloaded && DepotDownloader \"\$@\"" -- "$@"
+proot-distro login alpine --user $username --no-arch-warning --shared-tmp -- ash -c "cd \"$SET_DIR\" && DepotDownloader \"\$@\"" -- "$@"
 if [ $? -eq 0 ]; then
 	if [ "$#" -ne 0 ] && ! echo "$@" | grep -qE '(^|\s)(-V|--version)($|\s)'; then
-		print_green "Download finished! Check the 'Downloads/depotdownloaded' folder in your file manager app."
+		print_green "Download finished! Check the '${SET_DIR#/storage/emulated/0/}' folder in your file manager app."
 	fi
 else
 	print_red "Failed"
