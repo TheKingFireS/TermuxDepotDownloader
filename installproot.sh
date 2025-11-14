cb8478fe35c3a3688e8cf20893ce9c65e44d8a21
TheKingFireS <71936968+TheKingFireS@users.noreply.github.com>
2025-11-14 22:18:54 +0100

    installproot.sh: removed useless commands

--- installproot.sh
+++ installproot.sh
@@ -27,9 +27,7 @@ installersetup() {
 	proot-distro login alpine --shared-tmp -- apk cache clean
 	(set +o pipefail; rm "$installed_rootfs"/alpine/var/cache/apk/*.apk)
 	# add user
-	proot-distro login alpine --shared-tmp -- addgroup storage
 	proot-distro login alpine --shared-tmp -- adduser -G wheel -D $username
-	proot-distro login alpine --shared-tmp -- adduser $username storage
 	echo "permit nopass :wheel as root" > "$installed_rootfs"/alpine/etc/doas.d/doas.conf
 	chmod u-w  "$installed_rootfs"/alpine/etc/doas.d/doas.conf
 	# setup storage and directory for depotdownloader
